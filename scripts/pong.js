c19f5db11281cd46b1da1438b809e7e6ff537c9c
Sahil Mehta <samehta91@gmail.com>
2016-10-05 19:11:29 -0700

    Animate the ball to bounce off walls and paddles

--- scripts/pong.js
+++ scripts/pong.js
@@ -1,8 +1,8 @@
 var animate = window.requestAnimationFrame ||
 		window.webkitRequestAnimationFrame ||
-		window.mozRequestAnimationFrame ||
-		window.oRequestAnimationFrame ||
-		window.msRequestAnimationFrame ||
+		window.mozRequestAnimationFrame    ||
+		window.oRequestAnimationFrame      ||
+		window.msRequestAnimationFrame     ||
 		function(callback) { window.setTimeout(callback, 1000/60) };
 
 var canvas = document.createElement('canvas');
@@ -36,6 +36,7 @@ var step = function() {
 
 var update = function() {
 	player.update();
+	ball.update(player.paddle, computer.paddle);
 };
 
 function Paddle(x, y, width, height) {
@@ -45,22 +46,6 @@ function Paddle(x, y, width, height) {
 	this.height = height;
 	this.x_speed = 0;
 	this.y_speed = 0;
-}
-
-function Player() {
-	this.paddle = new Paddle(765, 210, 10, 80);
-}
-
-function Computer() {
-	this.paddle = new Paddle(25, 210, 10, 80);
-}
-
-function Ball(x, y) {
-	this.x = x;
-	this.y = y;
-	this.x_speed = 0;
-	this.y_speed = 3;
-	this.radius = 5;
 }
 
 Paddle.prototype.render = function() {
@@ -83,6 +68,10 @@ Paddle.prototype.move = function(x, y) {
 	}
 };
 
+function Player() {
+	this.paddle = new Paddle(765, 210, 10, 80);
+}
+
 Player.prototype.render = function() {
 	this.paddle.render();
 };
@@ -100,9 +89,21 @@ Player.prototype.update = function() {
 	}
 };
 
+function Computer() {
+	this.paddle = new Paddle(25, 210, 10, 80);
+}
+
 Computer.prototype.render = function() {
 	this.paddle.render();
 };
+
+function Ball(x, y) {
+	this.x = x;
+	this.y = y;
+	this.x_speed = 3;
+	this.y_speed = 0;
+	this.radius = 5;
+}
 
 Ball.prototype.render = function() {
 	context.beginPath();
@@ -110,6 +111,37 @@ Ball.prototype.render = function() {
 	context.fillStyle = "#E04747";
 	context.fill();
 };
+
+Ball.prototype.update = function(paddle1, paddle2) {
+	this.x += this.x_speed;
+	this.y += this.y_speed;
+	var top_x = this.x - 5;
+	var top_y = this.y - 5;
+	var bottom_x = this.x + 5;
+	var bottom_y = this.y + 5;
+
+	if (this.y - 5 < 0) { // hitting top wall
+		this.y = 5;
+		this.y_speed = -this.y_speed;
+	} else if (this.y + 5 > 500) { // hitting bottom wall
+		this.y = 495;
+		this.y_speed = -this.y_speed;
+	}
+	
+	if (top_x > 400) {
+		if (top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x && top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y) {
+			this.x_speed = -3;
+			this.y_speed += (paddle1.y_speed / 2);
+			this.x += this.x_speed;
+		}
+	} else {
+		if (top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x && top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y) {
+			this.x_speed = 3;
+			this.y_speed += (paddle2.y_speed / 2);
+			this.x += this.x_speed;
+		}
+	}
+}
 
 var keysDown = {};
 
